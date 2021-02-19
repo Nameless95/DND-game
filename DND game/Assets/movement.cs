c5be742865f230fc63194c8159d77640c5505543
NamelessGod95 <72949814+NamelessGod95@users.noreply.github.com>
2021-02-19 18:48:07 -0500

    can make new guns now yeaaa

--- DND game/Assets/movement.cs
+++ DND game/Assets/movement.cs
@@ -9,23 +9,35 @@ public class movement : MonoBehaviour
 {
     Control left;
     Rigidbody2D rb;
-   public Camera cm;
+    public Camera cm;
+    public GunTemplate gun;
+    float gunCooldown;
 
-    public float mag;
     void Awake()
     {
         left = new Control();
         rb = this.GetComponent<Rigidbody2D>();
         left.cont.shoot.performed += ctx => shoot();
+        gunCooldown = gun.fireRate;
         Debug.Log("working");
+    }
+
+    private void Update()
+    {
+        gunCooldown -= Time.deltaTime;
     }
 
     void shoot()
     {
+        Debug.Log(gunCooldown);
+        if (gunCooldown > 0) 
+            return;
+        gunCooldown = gun.fireRate;
         Vector2 power;
         power = this.transform.position - cm.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Debug.Log(gun.gunName + " was used");
         power.Normalize();
-        power *= mag;
+        power *= gun.knockBack;
         rb.AddForce(power);
     }
 
