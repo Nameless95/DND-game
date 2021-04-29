2ff5bbf4e9868fbb0fdcd86c8ee328876d255afe
msnider <msnider@livoniawarriors.org>
2021-04-29 19:35:18 -0400

    Settings Menu
    Functionality was added to allow the player to switch between the pause/setting menu and the main menu.
    Volume bar was added to pause menu, functionally works.
    -this has cause one error (UnassignedReferenceExpectation), it appears to have no affect on game play or any thing else for that matter.

---
+++ DND game/.history/Assets/UIScripts/UISystem_20210428214704.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UISystem : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI toggleText;
+
+    public void StartGame(){
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void startLobby(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void Toggle(){
+        if(toggleText.text == "On"){
+            toggleText.text = "Off";
+        }
+        else{
+            toggleText.text = "On";
+        }
+    }
+}
--- DND game/Assets/UIScripts/UISystem.cs
+++ DND game/Assets/UIScripts/UISystem.cs
@@ -7,17 +7,21 @@ using TMPro;
 public class UISystem : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI toggleText;
-
+   
     public void StartGame(){
         SceneManager.LoadScene(1);
     }
+
+    public void QuitGame(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
     public void startLobby(){
         SceneManager.LoadScene(0);
     }
-    public void startSettings(){
-        
-    }
 
+   
     public void Toggle(){
         if(toggleText.text == "On"){
             toggleText.text = "Off";
