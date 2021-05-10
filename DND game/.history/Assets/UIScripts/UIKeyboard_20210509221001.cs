574b826af736aeac9fb6557bf0447a50cb37e800
msnider <msnider@livoniawarriors.org>
2021-05-09 22:27:50 -0400

    Pause Menu Update
    escape now toggles the pause menu.
    The pause menu now freezes the game.
    Buttons for load and new game have been added.

---
+++ DND game/.history/Assets/UIScripts/UIKeyboard_20210509221001.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIKeyboard : MonoBehaviour
+{
+    public static bool curPaus = false;
+    public GameObject pauseMenu;
+    public GameObject headMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            Debug.Log("pressed");
+            if (curPaus){
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+    void Resume(){
+        pauseMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        Time.timeScale = 1f;
+        curPaus = false;
+    }
+    void Pause(){
+        pauseMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        Time.timeScale = 0f;
+        curPaus = true;
+    }
+}
