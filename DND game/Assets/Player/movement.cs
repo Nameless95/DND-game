d3f9088ef75ee7ff626d2729df0193c189524976
zaki <ahmedzakariya355@gmail.com>
2021-05-16 19:43:36 -0400

    Fixed cooldown bug, so closed to getting this fixed
    Eh, screw it. Tomorrow

--- DND game/Assets/Player/movement.cs
+++ DND game/Assets/Player/movement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 using TMPro;
 
@@ -14,93 +15,99 @@ public class movement : MonoBehaviour
     public Camera cm;
     public GunTemplate gun;
     float gunCooldown;
-    public Slider slideCooldown; 
+    public Slider slideCooldown;
     public GameObject bulletPrefab;
-    public GameObject GunSprite; 
-    bool reload=false;
-    bool knockback=true;
+    public GameObject GunSprite;
+    bool reload = false;
+    bool knockback = true;
+    public Tilemap grid;
 
     // Function run on startup
     void Awake()
     {
+        // Iniltalization
         left = new Control();
         rb = this.GetComponent<Rigidbody2D>();
+        grid = GameObject.Find("Tilemap").GetComponent<Tilemap>();
+
+        Vector3Int tmp = new Vector3Int((int)rb.position[0], (int)rb.position[1], 0 );
+        Debug.Log(tmp);
+        var tmp2 = grid.GetTile(tmp);
+        Debug.Log(tmp2);
         left.cont.shoot.performed += ctx => shoot();
-        left.cont.Holdback.performed += ctx => knockback=false;
-        left.cont.Holdback.canceled += ctx => knockback=true;
-        left.cont.reload.performed += ctx => {
-            if(reload==false){
-                Debug.Log("STOP");
+        left.cont.Holdback.performed += ctx => knockback = false;
+        left.cont.Holdback.canceled += ctx => knockback = true;
+        left.cont.reload.performed += ctx =>
+        {
+            if (reload == false)
+            {
                 gunCooldown = gun.reloadTime;
-                reload=true;
+                reload = true;
             }
         };
         gunCooldown = gun.fireRate;
-        GunSprite.GetComponent<SpriteRenderer>().sprite=gun.GunSprite;
-        Debug.Log("working");
+        GunSprite.GetComponent<SpriteRenderer>().sprite = gun.GunSprite;
 
     }
 
     private void Update()
     {
         // GUN LOGIC
-        gunCooldown = (gunCooldown >=0) ? (gunCooldown-Time.deltaTime): 0 ;
+        gunCooldown = (gunCooldown >= 0) ? (gunCooldown - Time.deltaTime) : 0;
         // Gun doens't got ammo: so do this instead
-        if(reload){
-            slideCooldown.value = gunCooldown/gun.reloadTime;
-            Debug.Log("reload");
+        if (reload)
+        {
+            slideCooldown.value = gunCooldown / gun.reloadTime;
             bool onGround = true;
-            if(onGround){
+            if (onGround)
+            {
                 // Ready to reload
-                if(gunCooldown == 0){
-                    Debug.Log("COOLDOWN");
+                if (gunCooldown == 0)
+                {
                     gun.ammo = gun.magSize;
                     reload = false;
                 }
             }
             // If not ground, then player isn't reloading: as such, just set the cooldown to this to rset it.
-            else {
-            slideCooldown.value = gunCooldown/gun.fireRate;
-                Debug.Log("Not on ground");
+            else
+            {
+                Debug.Log("Touc ground");
                 gunCooldown = gun.reloadTime;
             }
         }
+        else
+        {
+            Debug.Log("Cooldown");
+            slideCooldown.value = gunCooldown / gun.fireRate;
+            // slideCooldown.label = "Cooldown";
+        }
 
         // Displaying crap logic
-        Vector3 targetPostion = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()); 
+        Vector3 targetPostion = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         //Position.current.postion.currentvalue);
-        targetPostion.z = 0; 
-        GunSprite.transform.rotation = Quaternion.Euler(new Vector3 (0,0,Vector3.SignedAngle(Vector3.right, targetPostion - this.transform.position, Vector3.forward))); 
+        targetPostion.z = 0;
+        GunSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Vector3.SignedAngle(Vector3.right, targetPostion - this.transform.position, Vector3.forward)));
+
 
     }
 
     void shoot()
     {
-        Debug.Log(knockback);
-        if (gunCooldown > 0 && reload) {
+        if (gunCooldown > 0 || reload)
+        {
             return;
         }
-        // else if (gun.ammo <=0){
-        //     Debug.Log("NICE");
-        //     gunCooldown = gun.reloadTime;
-        //     reload=true;
-        //     return;
-        // }
-            
-        // Do I got ammo to shoot?
-        // If so, shoot
-        // Else, am I on ground?
-        // If so, run reload time
-        // If running reload time, if time 
-        if(gun.ammo >0){
+        if (gun.ammo > 0)
+        {
             gun.ammo--;
             gunCooldown = gun.fireRate;
             Vector2 power;
             power = this.transform.position - cm.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            Debug.Log(gun.gunName + " was used");
+            // Debug.Log(gun.gunName + " was used");
             power.Normalize();
             power *= gun.knockBack;
-            if(knockback){
+            if (knockback)
+            {
                 rb.velocity = power;
             }
 
@@ -108,13 +115,13 @@ public class movement : MonoBehaviour
             GameObject tempObject = BPS.instance.GetPooledObject("Bullet");
             tempObject.GetComponent<gun>().WakeUp((Vector2)transform.position, power, gun.damage);
         }
-        
+
     }
 
 
     private void OnEnable()
     {
-        left.cont.Enable(); 
+        left.cont.Enable();
     }
 
     private void OnDisable()
