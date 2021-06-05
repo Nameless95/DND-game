bafaec91ed8d24bc9e8213e0c94bfb0f8cc68544
msnider <msnider@livoniawarriors.org>
2021-06-05 03:14:39 -0400

    not much

---
+++ DND game/.history/Assets/Player/movement_20210605030855.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class movement : MonoBehaviour
+{
+    Control left;
+    Rigidbody2D rb;
+    public Camera cm;
+    public GunTemplate gun;
+    float gunCooldown;
+    public Slider slideCooldown; 
+    public GameObject bulletPrefab;
+    public GameObject GunSprite;
+    public Camera cam; 
+
+    void Awake()
+    {
+        left = new Control();
+        rb = this.GetComponent<Rigidbody2D>();
+        left.cont.shoot.performed += ctx => shoot();
+        gunCooldown = gun.fireRate;
+        Debug.Log("working");
+
+    }
+
+    private void Update()
+    {
+        gunCooldown = (gunCooldown >=0) ? (gunCooldown-Time.deltaTime): 0 ;
+        slideCooldown.value = gunCooldown/gun.fireRate;
+        Vector3 targetPostion = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue()); 
+            //Position.current.postion.currentvalue);
+        targetPostion.z = 0; 
+        GunSprite.transform.rotation = Quaternion.Euler(new Vector3 (0,0,Vector3.SignedAngle(Vector3.right, targetPostion - this.transform.position, Vector3.forward))); 
+
+    }
+
+    void shoot()
+    {
+        Debug.Log(  gunCooldown/gun.fireRate);
+        if (gunCooldown > 0) 
+            return;
+        gunCooldown = gun.fireRate;
+        Vector2 power;
+        power = this.transform.position - cm.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Debug.Log(gun.gunName + " was used");
+        power.Normalize();
+        power *= gun.knockBack;
+        rb.velocity = power;
+
+        //Bulletstuff
+        GameObject tempObject = BPS.instance.GetPooledObject("Bullet");
+        tempObject.GetComponent<gun>().WakeUp((Vector2)transform.position, power);
+    }
+
+
+    private void OnEnable()
+    {
+        left.cont.Enable(); 
+    }
+
+    private void OnDisable()
+    {
+        left.cont.Disable();
+    }
+
+}
