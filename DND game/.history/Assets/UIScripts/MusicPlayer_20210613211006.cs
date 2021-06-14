1c127a9ee2380fc709d373730949738705e555b6
msnider <msnider@livoniawarriors.org>
2021-06-13 21:54:15 -0400

    Volume works good :grin:
    :grin: :grinning: :smile: :smirk: :smiley: :joy_cat: :smirk_cat: :smile_cat:

---
+++ DND game/.history/Assets/UIScripts/MusicPlayer_20210613211006.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource Musi;
+    public Slider volumer;
+    private float musicVolume = 1f;
+
+    void Start()
+    {
+       Musi.Play();
+       musicVolume = PlayerPrefs.getFloat("volume");
+       Musi.volume = musicVolume;
+       volumer.value = musicVolume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(musicVolume);
+        Musi.volume = musicVolume;
+        PlayerPrefs.setFloat("volume", musicVolume);
+    }
+
+        public void updateVolume(float volume ){
+        musicVolume = volume;
+    }
+}
--- DND game/Assets/UIScripts/MusicPlayer.cs
+++ DND game/Assets/UIScripts/MusicPlayer.cs
@@ -1,27 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Audio;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     public AudioSource Musi;
-
+    public Slider volumer;
     private float musicVolume = 1f;
+
     void Start()
     {
        Musi.Play();
+       musicVolume = PlayerPrefs.GetFloat("volume");
+       Musi.volume = musicVolume;
+       volumer.value = musicVolume;
     }
 
     // Update is called once per frame
-    
-    public void updateVolume(float volume ){
-        musicVolume = volume;
-    }
     void Update()
     {
-        //Debug.Log(musicVolume);
+        Debug.Log(musicVolume);
         Musi.volume = musicVolume;
+        PlayerPrefs.SetFloat("volume", musicVolume);
+    }
+
+        public void updateVolume(float volume ){
+        musicVolume = volume;
     }
 }
