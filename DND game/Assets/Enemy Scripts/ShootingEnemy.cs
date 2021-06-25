2db508a9017ab855853863813f74c8013320b8b7
Kirk Daniel Caponpon <caponponkirk24@gmail.com>
2021-06-25 17:59:45 -0400

    Revert "More Important Changes"
    This reverts commit ffc4daa658357250763cfe9885f666244d4c7eaf.

--- DND game/Assets/Enemy Scripts/ShootingEnemy.cs
+++ DND game/Assets/Enemy Scripts/ShootingEnemy.cs
@@ -43,7 +43,6 @@ public class ShootingEnemy : MonoBehaviour
         }
         else if (distanceFromPlayer > shootingRange)
         {
-            //Debug.Log(StopMoving);
             StopMoving.speed = speed;  //sets speed back to normal 
             transform.Translate(Vector2.right * StopMoving.speed * 1 * Time.deltaTime);
             IsShooting = false;
--- DND game/Assets/Guns/GunTemplate.cs
+++ DND game/Assets/Guns/GunTemplate.cs
@@ -11,11 +11,5 @@ public class GunTemplate : ScriptableObject
     public float fireRate;
     public float knockBack;
     public Sprite GunSprite; 
-    // Sorry to any gun heads, I don't know the term for
-    // number of bullets in a gun. If you can correct me
-    // so this term can work for shotguns, pistols, and RPGS
-    public uint magSize;
-    public uint ammo;
-    // time to reload: it is in seconds.
-    public float reloadTime;
+
     }
