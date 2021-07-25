6e22cb3598573e18ed0c67f366110b8f24ae7c11
zaki <ahmedzakariya355@gmail.com>
2021-07-25 14:46:29 -0400

    added basis
    Next step is to have it select it select random guns

---
+++ DND game/Assets/NPC/Vendor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
+using UnityEditor.PackageManager;
+
+
+public class Vendor : MonoBehaviour{
+   
+   public List<GunTemplate> guns;
+   public int choices = 2;
+    void Start()
+    {
+
+        Debug.Log(guns.Count);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
