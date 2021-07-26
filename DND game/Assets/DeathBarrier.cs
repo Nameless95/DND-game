f2b79ce7ecf1de3bb8e0fa96232c9b828c4992ef
Kirk Daniel Caponpon <caponponkirk24@gmail.com>
2021-07-26 18:37:08 -0400

    Death Barrier

---
+++ DND game/Assets/DeathBarrier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour
+{
+    public int damage; 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+    }
+
+}
---
+++ DND game/Assets/Player/PlayerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 40;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    public GameObject deathEffect;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+    private void Update()
+    {
+
+        if (currentHealth <= 0)
+        {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            FindObjectOfType<LevelManager>().GameOver();
+            Destroy(gameObject); 
+        }
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(5); 
+
+        }
+    }
+}
+
